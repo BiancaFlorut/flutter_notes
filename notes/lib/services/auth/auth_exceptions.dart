7083ed9d9a7bd649b502edf05473c01055630055
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-09-12 15:00:16 +0200

    Auth Service

---
+++ notes/lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,14 @@
+
+//login exceptions
+class InvalidCredentialsException implements Exception {}
+class LoginFailedException implements Exception {}
+
+//register exceptions
+
+class WeakPasswordException implements Exception {}
+class EmailAlreadyInUseException implements Exception {}
+class InvalidEmailException implements Exception {}
+
+//generic exceptions
+class GenericAuthException implements Exception {}
+class UserNotLoggedInException implements Exception {}
---
+++ notes/lib/services/auth/auth_service.dart
@@ -0,0 +1,29 @@
+import 'auth_provider.dart';
+import 'auth_user.dart';
+
+class AuthService implements AuthProvider {
+  final AuthProvider provider;
+
+  const AuthService(this.provider);
+
+  @override
+  Future<void> sendEmailVerification() => provider.sendEmailVerification();
+
+  @override
+  Future<void> logOut() => provider.logOut();
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) => provider.createUser(email: email, password: password);
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) => provider.logIn(email: email, password: password);
+
+  @override
+  AuthUser? get currentUser => provider.currentUser;
+}
