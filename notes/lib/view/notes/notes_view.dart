c41b7279b646ea12077c130a150c417a831ecfa9
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-09-20 13:58:40 +0200

    Displaying Notes in Notes View

--- notes/lib/view/notes/notes_view.dart
+++ notes/lib/view/notes/notes_view.dart
@@ -12,25 +12,14 @@ class NotesView extends StatefulWidget {
 }
 
 class _NotesViewState extends State<NotesView> {
-
   late final NotesService _notesService;
 
-  String get userEmail =>
-      AuthService
-          .firebase()
-          .currentUser!
-          .email!;
+  String get userEmail => AuthService.firebase().currentUser!.email!;
 
   @override
   void initState() {
     _notesService = NotesService();
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    _notesService.close();
-    super.dispose();
   }
 
   @override
@@ -52,8 +41,8 @@ class _NotesViewState extends State<NotesView> {
                 if (shouldLogout) {
                   await AuthService.firebase().logOut();
                   if (context.mounted) {
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                        loginRoute, (_) => false);
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil(loginRoute, (_) => false);
                   }
                 }
                 break;
@@ -78,9 +67,28 @@ class _NotesViewState extends State<NotesView> {
                 builder: (context, snapshot) {
                   switch (snapshot.connectionState) {
                     case ConnectionState.waiting:
-                      case ConnectionState.active:
-                      return const Text('Waiting for all notes..');
-                    default: return const CircularProgressIndicator();
+                    case ConnectionState.active:
+                      if (snapshot.hasData) {
+                        final allNotes = snapshot.data as List<DatabaseNote>;
+                        return ListView.builder(
+                          itemCount: allNotes.length,
+                          itemBuilder: (context, index) {
+                            final note = allNotes[index];
+                            return ListTile(
+                              title: Text(
+                                note.text,
+                                maxLines: 1,
+                                softWrap: true,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            );
+                          },
+                        );
+                      } else {
+                        return const CircularProgressIndicator();
+                      }
+                    default:
+                      return const CircularProgressIndicator();
                   }
                 },
               );
@@ -88,25 +96,29 @@ class _NotesViewState extends State<NotesView> {
               return const CircularProgressIndicator();
           }
         },
-      )
-      ,
+      ),
     );
   }
 }
 
 Future<bool> showLogoutDialog(BuildContext context) {
-  return showDialog(context: context,
+  return showDialog(
+      context: context,
       builder: (context) {
         return AlertDialog(
           title: const Text("Sign out"),
           content: const Text("Are you sure you want to sign out?"),
           actions: [
-            TextButton(onPressed: () {
-              Navigator.of(context).pop(false);
-            }, child: const Text("Cancel")),
-            TextButton(onPressed: () {
-              Navigator.of(context).pop(true);
-            }, child: const Text("Log out"))
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text("Cancel")),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text("Log out"))
           ],
         );
       }).then((value) => value ?? false);
