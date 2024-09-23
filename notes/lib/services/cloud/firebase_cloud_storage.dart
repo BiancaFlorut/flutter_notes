8d7600edf8f252b95ce97dad22b02cbaf15d7c4e
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-09-23 20:47:09 +0200

    Migrating to our Firestore Service

--- notes/lib/services/cloud/firebase_cloud_storage.dart
+++ notes/lib/services/cloud/firebase_cloud_storage.dart
@@ -20,8 +20,7 @@ class FirebaseCloudStorage {
   Future<void> updateNote({
     required String documentId,
     required String text,
-  }
-  ) async {
+  }) async {
     try {
       await notes.doc(documentId).update({
         textFieldName: text,
@@ -32,13 +31,9 @@ class FirebaseCloudStorage {
   }
 
   Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) =>
-      notes
-          .snapshots()
-          .map(
-              (event) => event.docs
-                .map((doc) => CloudNote.fromSnapshot(doc))
-                .where((note) => note.ownerUserId == ownerUserId)
-          );
+      notes.snapshots().map((event) => event.docs
+          .map((doc) => CloudNote.fromSnapshot(doc))
+          .where((note) => note.ownerUserId == ownerUserId));
 
   Future<Iterable<CloudNote>> getNotes({
     required String ownerUserId,
@@ -48,26 +43,24 @@ class FirebaseCloudStorage {
           .where(ownerUserIdFieldName, isEqualTo: ownerUserId)
           .get()
           .then(
-            (value) => value.docs.map(
-              (doc) {
-                return CloudNote(
-                  documentId: doc.id,
-                  ownerUserId: doc.data()[ownerUserIdFieldName] as String,
-                  text: doc.data()[textFieldName] as String,
-                );
-              },
-            ),
+            (value) => value.docs.map((doc) => CloudNote.fromSnapshot(doc)),
           );
     } catch (error) {
       throw CouldNotGetAllNotesException();
     }
   }
 
-  void createNewNote({required String ownerUserId}) async {
-    await notes.add({
+  Future<CloudNote> createNewNote({required String ownerUserId}) async {
+    final document = await notes.add({
       ownerUserIdFieldName: ownerUserId,
       textFieldName: '',
     });
+    final fetchedNote = await document.get();
+    return CloudNote(
+      documentId: fetchedNote.id,
+      ownerUserId: ownerUserId,
+      text: '',
+    );
   }
 
   static final FirebaseCloudStorage _shared =
--- notes/lib/view/notes/notes_view.dart
+++ notes/lib/view/notes/notes_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:notes/services/auth/auth_service.dart';
-import 'package:notes/services/crud/notes_service.dart';
+import 'package:notes/services/cloud/firebase_cloud_storage.dart';
+// import 'package:notes/services/crud/notes_service.dart';
 import 'package:notes/view/notes/notes_list_view.dart';
 import '../../constants/routs.dart';
 import '../../enums/menu_actions.dart';
+import '../../services/cloud/cloud_note.dart';
 import '../../utilities/dialog/logout_dialog.dart';
 
 class NotesView extends StatefulWidget {
@@ -14,87 +16,126 @@ class NotesView extends StatefulWidget {
 }
 
 class _NotesViewState extends State<NotesView> {
-  late final NotesService _notesService;
+  // late final NotesService _notesService;
+  late final FirebaseCloudStorage _notesService;
 
-  String get userEmail => AuthService.firebase().currentUser!.email;
+  // String get userEmail =>
+  //     AuthService
+  //         .firebase()
+  //         .currentUser!
+  //         .email;
+
+  String get userId => AuthService.firebase().currentUser!.id;
 
   @override
   void initState() {
-    _notesService = NotesService();
+    _notesService = FirebaseCloudStorage();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Your Notes"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed(createUpdateNoteRoute);
-            },
-            icon: const Icon(Icons.add),
-          ),
-          PopupMenuButton<MenuAction>(onSelected: (value) async {
-            switch (value) {
-              case MenuAction.logout:
-                final shouldLogout = await showLogoutDialog(context);
-                if (shouldLogout) {
-                  await AuthService.firebase().logOut();
+        appBar: AppBar(
+          title: const Text("Your Notes"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(createUpdateNoteRoute);
+              },
+              icon: const Icon(Icons.add),
+            ),
+            PopupMenuButton<MenuAction>(onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogoutDialog(context);
+                  if (shouldLogout) {
+                    await AuthService.firebase().logOut();
 
-                  Navigator.of(context)
-                      .pushNamedAndRemoveUntil(loginRoute, (_) => false);
-                }
-                break;
-            }
-          }, itemBuilder: (context) {
-            return const [
-              PopupMenuItem<MenuAction>(
-                value: MenuAction.logout,
-                child: Text("Log out"),
-              )
-            ];
-          })
-        ],
-      ),
-      body: FutureBuilder(
-        future: _notesService.getOrCreateUser(email: userEmail),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return StreamBuilder(
-                stream: _notesService.allNotes,
-                builder: (context, snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.waiting:
-                    case ConnectionState.active:
-                      if (snapshot.hasData) {
-                        final allNotes = snapshot.data as List<DatabaseNote>;
-                        return NotesListView(
-                            notes: allNotes,
-                            onTap: (note) {
-                              Navigator.of(context).pushNamed(
-                                createUpdateNoteRoute,
-                                arguments: note,
-                              );
-                            },
-                            onDeleteNote: (note) async {
-                              await _notesService.deleteNote(id: note.id);
-                            });
-                      } else {
-                        return const CircularProgressIndicator();
-                      }
-                    default:
-                      return const CircularProgressIndicator();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil(loginRoute, (_) => false);
                   }
-                },
-              );
-            default:
-              return const CircularProgressIndicator();
-          }
-        },
-      ),
+                  break;
+              }
+            }, itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text("Log out"),
+                )
+              ];
+            })
+          ],
+        ),
+        body: StreamBuilder(
+          stream: _notesService.allNotes(ownerUserId: userId),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+              case ConnectionState.active:
+                if (snapshot.hasData) {
+                  final allNotes = snapshot.data as Iterable<CloudNote>;
+                  return NotesListView(
+                      notes: allNotes.toList(),
+                      onTap: (note) {
+                        Navigator.of(context).pushNamed(
+                          createUpdateNoteRoute,
+                          arguments: note,
+                        );
+                      },
+                      onDeleteNote: (note) async {
+                        await _notesService.deleteNote(documentId: note.documentId);
+                      });
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              default:
+                return const CircularProgressIndicator();
+            }
+          },
+        ),
     );
   }
 }
+
+//
+// {
+//
+// const fb = FutureBuilder(
+//   future: _notesService.getOrCreateUser(email: userEmail),
+//   builder: (context, snapshot) {
+//     switch (snapshot.connectionState) {
+//       case ConnectionState.done:
+//         return StreamBuilder(
+//           stream: _notesService.allNotes,
+//           builder: (context, snapshot) {
+//             switch (snapshot.connectionState) {
+//               case ConnectionState.waiting:
+//               case ConnectionState.active:
+//                 if (snapshot.hasData) {
+//                   final allNotes = snapshot.data as List<DatabaseNote>;
+//                   return NotesListView(
+//                       notes: allNotes,
+//                       onTap: (note) {
+//                         Navigator.of(context).pushNamed(
+//                           createUpdateNoteRoute,
+//                           arguments: note,
+//                         );
+//                       },
+//                       onDeleteNote: (note) async {
+//                         await _notesService.deleteNote(id: note.id);
+//                       });
+//                 } else {
+//                   return const CircularProgressIndicator();
+//                 }
+//               default:
+//                 return const CircularProgressIndicator();
+//             }
+//           },
+//         );
+//       default:
+//         return const CircularProgressIndicator();
+//     }
+//   },
+// )
+// ,}
